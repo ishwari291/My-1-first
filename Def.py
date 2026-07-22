0976cda8fb163b9fd3c822bb33fada61c85b3d46
ishwari291 <hitareishwari@gmail.com>
2026-07-22 18:31:38 +0530

    Implement password generation and retry logic

---
+++ Def.py
@@ -0,0 +1,37 @@
+LengthPasswd = input("What is the password length? ")
+
+16
+
+passwdFinal = join(random.sample (mergeApproaches, int (lengthPasswd)))
+
+17
+
+18
+
+print(passwdFinal)
+
+tryother = input("Try again? (y/n) ")
+
+19
+
+if tryother = "y":
+
+20
+
+print("\n")
+
+21
+
+genPass()
+
+22
+
+else:
+
+23
+
+print("\n")
+
+24
+
+exit()
