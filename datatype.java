636f0a5431c25c11cef8834fa91265e544148983
ishwari291 <hitareishwari@gmail.com>
2026-07-25 19:35:41 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
